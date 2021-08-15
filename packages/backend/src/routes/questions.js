bd6926e33e05fc23255e9de616f1715ce5108557
Narendra Joshi <48818291+joshinar@users.noreply.github.com>
2021-08-15 16:10:55 +0530

    added questions api

--- packages/backend/src/routes/questions.js
+++ packages/backend/src/routes/questions.js
@@ -9,15 +9,16 @@ var router = express.Router();
 var sendEmail=require('../services/mailservice');
 
 router.post('/', function(req, res, next) {
-  if(!req.body.category || !req.body.company || !req.body.question)
-        sendResponse(res,"Category,question and company are required",400);
+  let {category,question,company}=req.params;
+
+  if(!category || !company || !question)
+        sendResponse(res,{error:"Category,question and company are required"},400);
  // sendResponse(res,req.body,200);
   db.collection("questions").add({
-      id:"dd",
-      category: req.body.category,
-      company: req.body.company,
+      category: category,
+      company: company,
       created_at: formatedDate(new Date(),"dd/mm/yyyy"),
-      question:req.body.question
+      question:question
   })
   .then((docRef) => {
       //console.log("Document written with ID: ", docRef.id);
@@ -25,10 +26,41 @@ router.post('/', function(req, res, next) {
   })
   .catch((error) => {
       //console.error("Error adding document: ", error);
-      return res.status(500).json({ message: 'Add Question erro',error:error });
+      return res.status(500).json({ message: 'Add Question error',error:error });
   });
   
 });
 
+
+
+router.put('/:id', function(req, res, next) {
+  let id=req.params.id;
+
+  let {category,question,company}=req.body;
+
+  if(!id)
+        sendResponse(res,"Id is required",400);
+  else if(!category || !company || !question)
+        sendResponse(res,{error:"Category,question and company are required"},400);
+  // sendResponse(res,req.body,200);
+  let date=db.collection("questions").doc(id).created_at;
+  db.collection("questions").doc(id).set({
+      category: category,
+      company: company,
+      created_at: formatedDate(date!=null?new Date(date):new Date(),"DD/MM/yyyy"),
+
+      updated_at: formatedDate(new Date(),"DD/MM/yyyy"),
+      question:question
+  })
+  .then((docRef) => {
+  //console.log("Document written with ID: ", docRef.id);
+      return res.status(200).json({ message: 'Update Question success ' });
+  })
+  .catch((error) => {
+      //console.error("Error adding document: ", error);
+      return res.status(500).json({ message: 'Update Question error',error:JSON.stringify(error) });
+  });
+  
+});
 
 module.exports = router;
